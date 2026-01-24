0529a2fcab2f5e93538095cf917164c7bf0df221
Cristhian Esteves <132605397+Cristhianstevs@users.noreply.github.com>
2026-01-23 22:42:44 -0300

    updates

--- script/script.js
+++ script/script.js
@@ -22,14 +22,8 @@ function preloadAudios(audios) {
     return Promise.all(
         audios.map(audio => {
             return new Promise(resolve => {
-                const originalVolume = audio.volume;
-                audio.volume = 0;
-                audio.play().then(() => {
-                    audio.pause();
-                    audio.currentTime = 0;
-                    audio.volume = originalVolume;
-                    resolve();
-                });
+                audio.load();
+                audio.addEventListener("canplaythrough", resolve, { once: true });
             });
         })
     );
@@ -55,69 +49,69 @@ async function preloadEverything() {
 
     try {
         await preloadImages([
-            "../public/credit_img01.jpeg",
-            "../public/credit_img02.jpeg",
+            "./public/credit_img01.jpeg",
+            "./public/credit_img02.jpeg",
             "../public/credit_img03.jpeg",
-            "../public/credit_img04.jpeg",
-            "../public/credit_img05.jpeg",
-            "../public/credit_img06.jpeg",
-            "../public/credit_img07.jpeg",
-            "../public/credit_img08.jpeg",
-            "../public/credit_img09.jpeg",
-            "../public/credit_img10.jpeg",
-            "../public/credit_img11.jpeg",
-            "../public/credit_img12.jpeg",
-            "../public/credit_img13.jpeg",
-            "../public/credit_img14.jpeg",
-            "../public/credit_img15.jpeg",
-            "../public/credit_img16.jpeg",
-            "../public/credit_img17.jpeg",
-            "../public/credit_img18.jpeg",
-            "../public/credit_img19.jpeg",
-            "../public/credit_img20.jpeg",
-            "../public/background01_zelda.jpg",
-            "../public/background02_zelda.jpg",
-            "../public/background03_zelda.jpg",
-            "../public/background04_zelda.jpg",
-            "../public/background05_zelda.jpg",
-            "../public/background06_zelda.jpg",
-            "../public/background07_zelda.jpg",
-            "../public/background08_zelda.jpg",
-            "../public/background09_zelda.jpg",
-            "../public/background10_zelda.jpg",
-            "../public/background11_zelda.jpg",
-            "../public/background12_zelda.jpg",
-            "../public/background13_zelda.jpg",
-            "../public/background14_zelda.jpg",
-            "../public/background15_zelda.jpg",
-            "../public/background16_zelda.jpg",
-            "../public/background17_zelda.jpg",
-            "../public/background18_zelda.jpg",
-            "../public/background19_zelda.jpg",
-            "../public/background20_zelda.jpg",
-            "../public/background21_zelda.jpg",
-            "../public/background22_zelda.jpg",
-            "../public/background23_zelda.jpg",
-            "../public/background24_zelda.jpg",
-            "../public/background25_zelda.jpg",
-            "../public/background26_zelda.jpg",
-            "../public/background27_zelda.jpg",
-            "../public/background28_zelda.jpg",
-            "../public/reward06.jpg",
-            "../public/alert_icon.png",
-            "../public/korok01.png",
-            "../public/korok02.png",
-            "../public/korok03.png",
-            "../public/master_sword.png",
-            "../public/reward.png",
-            "../public/reward03.png",
-            "../public/reward04.png",
-            "../public/reward07.png",
-            "../public/reward08.png",
-            "../public/reward09.png",
-            "../public/rupee_icon.png",
-            "../public/korok02.webp",          
-            "../public/reward02.webp",          
+            "./public/credit_img04.jpeg",
+            "./public/credit_img05.jpeg",
+            "./public/credit_img06.jpeg",
+            "./public/credit_img07.jpeg",
+            "./public/credit_img08.jpeg",
+            "./public/credit_img09.jpeg",
+            "./public/credit_img10.jpeg",
+            "./public/credit_img11.jpeg",
+            "./public/credit_img12.jpeg",
+            "./public/credit_img13.jpeg",
+            "./public/credit_img14.jpeg",
+            "./public/credit_img15.jpeg",
+            "./public/credit_img16.jpeg",
+            "./public/credit_img17.jpeg",
+            "./public/credit_img18.jpeg",
+            "./public/credit_img19.jpeg",
+            "./public/credit_img20.jpeg",
+            "./public/background01_zelda.jpg",
+            "./public/background02_zelda.jpg",
+            "./public/background03_zelda.jpg",
+            "./public/background04_zelda.jpg",
+            "./public/background05_zelda.jpg",
+            "./public/background06_zelda.jpg",
+            "./public/background07_zelda.jpg",
+            "./public/background08_zelda.jpg",
+            "./public/background09_zelda.jpg",
+            "./public/background10_zelda.jpg",
+            "./public/background11_zelda.jpg",
+            "./public/background12_zelda.jpg",
+            "./public/background13_zelda.jpg",
+            "./public/background14_zelda.jpg",
+            "./public/background15_zelda.jpg",
+            "./public/background16_zelda.jpg",
+            "./public/background17_zelda.jpg",
+            "./public/background18_zelda.jpg",
+            "./public/background19_zelda.jpg",
+            "./public/background20_zelda.jpg",
+            "./public/background21_zelda.jpg",
+            "./public/background22_zelda.jpg",
+            "./public/background23_zelda.jpg",
+            "./public/background24_zelda.jpg",
+            "./public/background25_zelda.jpg",
+            "./public/background26_zelda.jpg",
+            "./public/background27_zelda.jpg",
+            "./public/background28_zelda.jpg",
+            "./public/reward06.jpg",
+            "./public/alert_icon.png",
+            "./public/korok01.png",
+            "./public/korok02.png",
+            "./public/korok03.png",
+            "./public/master_sword.png",
+            "./public/reward.png",
+            "./public/reward03.png",
+            "./public/reward04.png",
+            "./public/reward07.png",
+            "./public/reward08.png",
+            "./public/reward09.png",
+            "./public/rupee_icon.png",
+            "./public/korok02.webp",          
+            "./public/reward02.webp",          
         ]);
 
         await preloadAudios([
@@ -304,14 +298,25 @@ function wait(ms) {
 /* INIT */
 pressStart.style.display = "flex";
 
+/* PLAY WRAPPER */
+function handleStartInteraction() {
+    if (!assetsLoaded) return;
+    if (started) return;
+
+    // Áudio no clique
+    sound_start.currentTime = 0;
+    sound_start.play();
+
+    // Inicia a sequência
+    startGameSequence();
+}
+
 /* SEQUÊNCIA PRINCIPAL */
 async function startGameSequence() {
     if (!assetsLoaded) return;
     if (started) return;
     started = true;
-    
-    sound_start.currentTime = 0;
-    sound_start.play();
+
 
     // Fade-out Press Start
     pressStart.classList.add("fade-out");
@@ -422,8 +427,8 @@ async function startGameSequence() {
 }
 
 /* INPUT */
-startScreen.addEventListener("click", startGameSequence);
-startScreen.addEventListener("keydown", startGameSequence);
+startScreen.addEventListener("click", handleStartInteraction);
+startScreen.addEventListener("keydown", handleStartInteraction);
 
 
 
